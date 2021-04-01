d443d638059e3d7802847936b6b85ac18b853d2b
olena <melolena33@gmail.com>
2021-04-01 19:06:37 +0300

    Updates

---
+++ precache-manifest.5ecf85c3e0806fd0cbe644f38ed6ddad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1a95e7f23dc652a97ac93d0fff10ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ab736a9a1122c46200b",
+    "url": "./static/css/2.ee398134.chunk.css"
+  },
+  {
+    "revision": "387ab54d587d773cb603",
+    "url": "./static/css/main.65401fad.chunk.css"
+  },
+  {
+    "revision": "8ab736a9a1122c46200b",
+    "url": "./static/js/2.5027c62e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5027c62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387ab54d587d773cb603",
+    "url": "./static/js/main.43ad533d.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
